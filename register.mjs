189a0b48be39712cc63c74515ec1c5604d4fdd80
Benjamin Gwynn <git@benjamingwynn.com>
2024-04-08 18:14:52 +0100

    remove debug print

--- register.mjs
+++ register.mjs
@@ -5,6 +5,5 @@ import path from "node:path"
 import url from "node:url"
 
 const x = path.join(url.fileURLToPath(import.meta.url), "..", "./hooks.mjs")
-console.log("x:", x)
 
 register(url.pathToFileURL(x))
